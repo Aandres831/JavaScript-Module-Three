e481aa34fa4eadaebd6ce16c70af1468d59d5ee7
Aandres831 <andresseverino646@gmail.comm>
2025-06-16 10:07:22 -0500

    Actulización Normal

---
+++ Week_One/main.js
@@ -0,0 +1,40 @@
+// Nombrar una variable
+let name1="Andres";
+console.log("Hola ", name1);
+
+// Diferentes Tipos de datos
+let int = 20;
+let float = 15.5;
+let string = "La suma es"
+let suma = int + float;
+
+
+// Mostrando los datos
+console.log(int)
+console.log(float)
+console.log(string)
+console.log(int, " + ", float , string, suma )
+
+// Es una variable definida por el programa
+// let PI = 3.1416;
+// let PI = 25;
+
+// sin valor = Undefined
+let sin;
+console.log(sin)
+
+// NUll y Boolean
+// [Function: Boolean]
+// null
+let bo = Boolean;
+let nu = null;
+
+console.log(Boolean);
+console.log(null);
+
+// recopilación de datos
+let edad = window.prompt('Ingresa tu edad: ');
+console.log("Tu edad es: " + edad);
+
+
+
