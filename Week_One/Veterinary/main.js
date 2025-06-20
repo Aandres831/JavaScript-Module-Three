f096cb94701506fd8bb44be2026ecba72708bdc6
Aandres831 <andresseverino646@gmail.comm>
2025-06-20 07:48:59 -0500

    Veterinary Functions

--- Week_One/Veterinary/main.js
+++ Week_One/Veterinary/main.js
@@ -1,19 +1,42 @@
 
-function registrarMascota(){
-    let mascotas ={};
 
-    let nombre = prompt("Cual es el nombre de la mascota: ");
-    let especie = prompt("Cual es la especie de la mascota: ");
-    let edad = prompt("Cual es la edad de la mascota: ");
-    let vacunado = prompt("La mascota está vacunada: ");
+console.log("Hi, welcome to Veterinarymals");
 
-    mascotas.nombre = nombre;
-    mascotas.especie = especie;
-    mascotas.edad = edad;
-    mascotas.vacunado = vacunado;
+// 1. Data (Global list of pets)
+let pets = [];
 
-    return mascotas
-}
+// 2. Logic (Arrow Functions)
+
+// Function to register a new pet
+const registerPets = () => {
+    const name = prompt("Enter pet's name:");
+    const species = prompt("Enter the pet species (dog, cat, iguana):");
+    const age = parseInt(prompt("Enter the pet age:"), 10);
+    const vaccinated =
+    prompt("Is the pet vaccinated? (yes/no):").toLowerCase() === "yes";
+
+  // Add the pet to the list
+    pets.push({ name, species, age, vaccinated });
+    console.log(" Pet added successfully." + pets);
+};
+
+// Function to search a pet by name
+    const searchByName = () => {
+        const name = prompt("Enter name to search:");
+        const result = pets.find((p) => p.name.toLowerCase() === name.toLowerCase());
+
+    // Display result
+        result
+        ? console.log(" Pet found:", result)
+        : console.log(" Pet not found.");
+};
+
+
+
+
+
+
+
 
 
 
